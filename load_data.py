6aad453e39b853ed626da97b54715a1356374e12
Karan Shinde <skarans.0296@gmail.com>
2021-03-28 16:34:21 +0530

    Third commit

--- load_data.py
+++ load_data.py
@@ -9,12 +9,24 @@ class ReadArticles:
     
     def __init__(self):
         self.dataPath = r'C:\Users\Karan Shinde\OneDrive\Desktop\aws_new\article1.txt'
-        
+        self.stopWordPath = r"C:\Users\Karan Shinde\OneDrive\Desktop\aws_new\stopWords.txt"
+    
     def loadArticles(self):
         with open(self.dataPath,'r') as fl:
             text = fl.read()
         return text
     
-readObj = ReadArticles() 
-articleText = readObj.loadArticles()
-print("The text is \n\n{}".format(articleText))
+    def stopWordFromFile(self):
+        with open(self.stopWordPath,'r') as fl:
+            text = fl.read()
+        return text.split("\n")
+    
+    def loadArticlesFromDb(self):
+        pass
+    
+    def loadArticlesFromApi(self):
+        pass
+        
+#readObj = ReadArticles() 
+#articleText = readObj.loadArticles()
+#print("The text is \n\n{}".format(articleText))
